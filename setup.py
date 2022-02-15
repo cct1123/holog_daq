e6ea0d66023a7dc3e4001e2e654b0747c716eccf
chesmore <chesmore@uchicago.edu>
2022-02-15 11:30:07 -0600

    setup file

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def build_packages(base_dir, name_base):
                 [arr.append(rec) for rec in recursion]
     return arr
 
-packages = build_packages('holo_cmd')
+packages = build_packages('holog_daq/', 'holog_daq')
 
 setup(name='holog_daq',
       version=VERSION,
