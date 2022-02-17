fbb8c88a404a21fb3ed9609449d2e31d4323ac9f
chesmore <chesmore@uchicago.edu>
2022-02-17 16:23:54 -0600

    python3 version

--- setup.py
+++ setup.py
@@ -38,13 +38,14 @@ setup(name='holog_daq',
      )
 
 
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
+# # Install requirements
+# with pathlib.Path('requirements.txt').open() as requirements_txt:
+#     install_requires = [
+#         str(requirement)
+#         for requirement
+#         in pkg_resources.parse_requirements(requirements_txt)
+#     ]
 
-setuptools.setup(
-    install_requires=install_requires,
-)
+# setuptools.setup(
+#     install_requires=install_requires,
+# )
